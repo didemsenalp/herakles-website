5a236585c369854973e17d26dd77b76c9871ad43
Didem Şenalp <didemsenalp8@gmail.com>
2023-01-09 16:57:13 +0300

    sort completed

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-
-
-const express = require ("express");
-const bodyParser = require ("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date= require(__dirname+'/date.js');
+const date = require(__dirname + '/date.js');
 const app = express();
 
 app.set('view engine', 'ejs');
 
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({
+  extended: false
+}));
 
 app.use("/content", express.static("content"));
 app.use("/images", express.static("images"));
@@ -17,7 +17,9 @@ app.use("/css", express.static("css"));
 app.use("/js", express.static("js"));
 
 
-mongoose.connect("mongodb://localhost:27017/MyHeraklesDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/MyHeraklesDB", {
+  useNewUrlParser: true
+});
 
 mongoose.set('strictQuery', false);
 
@@ -33,31 +35,35 @@ const contactFormSchema = new mongoose.Schema({
 const ContactFormTable = mongoose.model("ContactFormTable", contactFormSchema);
 
 
-app.get("/", function(req,res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/manage-contact-informations", function(req,res) {
+app.get("/manage-contact-informations", function(req, res) {
   res.sendFile(__dirname + "/quadra-index.html");
 });
 
-app.get("/data-table-view", function(req,res){
+app.get("/data-table-view", function(req, res) {
 
-ContactFormTable.find({}, function(err, foundContactList) {
-  res.render("data-tables", {
-        contactList: foundContactList
-      });
-  res.sendFile(__dirname + "/elements-data-tables.html");
-});
+  ContactFormTable.find({}, function(err, foundContactList) {
+    console.log(foundContactList);
+    res.render("data-tables", {
+      contactList: foundContactList
+    });
+    res.sendFile(__dirname + "/elements-data-tables.html");
+  }).sort({name: 1});
+
+
+
 });
 
 app.post('/contact', (req, res) => {
 
-const day = date.getDate();
-const dayString = date.getDateAsString();
+  const day = date.getDate();
+  const dayString = date.getDateAsString();
 
 
-  const contactFormInfo = new ContactFormTable ({
+  const contactFormInfo = new ContactFormTable({
     name: req.body.name1,
     phone: req.body.phone,
     email: req.body.email1,
@@ -65,15 +71,15 @@ const dayString = date.getDateAsString();
     currentDate: day,
     currentDateString: dayString
 
-});
+  });
   contactFormInfo.save();
   res.redirect("/");
 
-  });
+});
 
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
